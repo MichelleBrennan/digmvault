1099d405a87ba6f538d99b73da888661bbf89237
Michelle Brennan <meb84z@gmail.com>
2012-01-20 01:26:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Digmvault::Application.config.secret_token = '7563494c7ab83f1facf7a22f5b5ffa1974d129f2bbafa11dea0a9b6200cb1f9c15b18bc292859c523caab65a2f174d6f13bcca2fb7983c67fc9418e06b8947e3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Digmvault::Application.config.session_store :cookie_store, key: '_digmvault_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Digmvault::Application.config.session_store :active_record_store
